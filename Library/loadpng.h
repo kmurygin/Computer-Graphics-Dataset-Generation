c2caf38906bf4e97ae810b63995562ad5fa857f6
Kacper Murygin <kacper.mur123@gmail.com>
2024-01-21 16:41:18 +0100

    removed redundant code and reorganised repo

--- Library/loadpng.h
+++
@@ -1,7 +0,0 @@
-#include "lodepng.h"
-
-void Swap_Image(unsigned char *img, unsigned w, unsigned h);
-void Load_Png(unsigned char** img, unsigned* w, unsigned* h, const char* filename);
-void Load_Png_Swap(unsigned char** img, unsigned* w, unsigned* h, const char* filename);
-
-#include "loadpng.c"
