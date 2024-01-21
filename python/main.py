c409d48fd67687a0de1cb628f2917970ad819e15
Kacper Murygin <kacper.mur123@gmail.com>
2024-01-21 11:00:37 +0100

    general refactoring- organized it into functions for better readability and maintainability

--- python/main.py
+++ python/main.py
@@ -3,69 +3,108 @@ from pygame.constants import *
 from OpenGL.GLU import *
 from OBJ import *
 
-pygame.init()
-viewport = (800,600)
-hx = viewport[0]/2
-hy = viewport[1]/2
-srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
+rotate = False
+move = False
 
-glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
-glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
-glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
-glEnable(GL_LIGHT0)
-glEnable(GL_LIGHTING)
-glEnable(GL_COLOR_MATERIAL)
-glEnable(GL_DEPTH_TEST)
-glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
 
-# LOAD OBJECT AFTER PYGAME INIT
-obj = OBJ("Football.obj", swapyz=True)
-obj.generate()
+def init():
+    pygame.init()
+    viewport = (800, 600)
+    hx, hy = viewport[0] / 2, viewport[1] / 2
+    srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
 
-clock = pygame.time.Clock()
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, (-40, 200, 100, 0.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
 
-glMatrixMode(GL_PROJECTION)
-glLoadIdentity()
-width, height = viewport
-gluPerspective(90.0, width/float(height), 1, 100.0)
-glEnable(GL_DEPTH_TEST)
-glMatrixMode(GL_MODELVIEW)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_COLOR_MATERIAL)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
 
-rx, ry = (0,0)
-tx, ty = (0,0)
-zpos = 5
-rotate = move = False
-while 1:
-    clock.tick(30)
-    for e in pygame.event.get():
-        if e.type == QUIT:
-            sys.exit()
-        elif e.type == KEYDOWN and e.key == K_ESCAPE:
-            sys.exit()
-        elif e.type == MOUSEBUTTONDOWN:
-            if e.button == 4: zpos = max(1, zpos-1)
-            elif e.button == 5: zpos += 1
-            elif e.button == 1: rotate = True
-            elif e.button == 3: move = True
-        elif e.type == MOUSEBUTTONUP:
-            if e.button == 1: rotate = False
-            elif e.button == 3: move = False
-        elif e.type == MOUSEMOTION:
-            i, j = e.rel
-            if rotate:
-                rx += i
-                ry += j
-            if move:
-                tx += i
-                ty -= j
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+def setup_projection(width, height):
+    glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
+    gluPerspective(90.0, width / float(height), 1, 100.0)
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_MODELVIEW)
 
-    # RENDER OBJECT
-    glTranslate(tx/20., ty/20., - zpos)
-    glRotate(ry, 1, 0, 0)
-    glRotate(rx, 0, 1, 0)
+
+def handle_input(rx, ry, tx, ty, zpos):
+    global rotate, move
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            sys.exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            handle_mouse_down(event, zpos)
+        elif event.type == MOUSEBUTTONUP:
+            handle_mouse_up(event)
+        elif event.type == MOUSEMOTION:
+            handle_mouse_motion(event, rx, ry, tx, ty)
+
+
+def handle_mouse_down(event, zpos):
+    global rotate, move
+    if event.button == 4:
+        zpos[0] = max(1, zpos[0] - 1)
+    elif event.button == 5:
+        zpos[0] += 1
+    elif event.button == 1:
+        rotate = True
+    elif event.button == 3:
+        move = True
+
+
+def handle_mouse_up(event):
+    global rotate, move
+    if event.button == 1:
+        rotate = False
+    elif event.button == 3:
+        move = False
+
+
+def handle_mouse_motion(event, rx, ry, tx, ty):
+    global rotate, move
+    i, j = event.rel
+    if rotate:
+        rx[0] += i
+        ry[0] += j
+    if move:
+        tx[0] += i
+        ty[0] -= j
+
+
+def render_object(obj, tx, ty, zpos, rx, ry):
+    glLoadIdentity()
+    glTranslate(tx[0] / 20., ty[0] / 20., -zpos[0])
+    glRotate(ry[0], 1, 0, 0)
+    glRotate(rx[0], 0, 1, 0)
     obj.render()
 
-    pygame.display.flip()
+
+def main():
+    init()
+    obj = OBJ("Football.obj", swapyz=True)
+    obj.generate()
+
+    clock = pygame.time.Clock()
+    width, height = 800, 600
+    setup_projection(width, height)
+
+    rx, ry, tx, ty = [0], [0], [0], [0]
+    zpos = [5]
+
+    while True:
+        clock.tick(30)
+        handle_input(rx, ry, tx, ty, zpos)
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        render_object(obj, tx, ty, zpos, rx, ry)
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
